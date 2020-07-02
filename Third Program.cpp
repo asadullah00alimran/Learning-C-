6b5e946aa4a424047dce7df0a0524203e1397596
Asadullah Al Imran <66588286+asadullah00alimran@users.noreply.github.com>
2020-07-02 09:32:45 +0600

    Add files via upload

---
+++ Third Program.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+    //Variable declaration,initialization value and print the value
+
+    //int num1,num1;
+    //int num1 = 10,num2 = 20;
+    int num1;
+    int num2;
+    num1 = 10;
+    num2 = 20;
+
+
+    //float num3,num4;
+    //int num3 = 30,num4 = 40;
+    float num3;
+    float num4;
+    num3=30.345;
+    num4=40.543;
+
+    //double num5,num6;
+    //double num5 = 50,num6 = 60;
+    double num5;
+    double num6;
+    num5=50.9834567;
+    num6=60.3566779;
+
+    //char ch1,ch2;
+    //char ch1 = 'a',ch2 = 'b'
+    char ch1;
+    char ch2;
+    ch1 = 'a';
+    ch2 = 'b';
+
+    cout<<"Number 1 = "<<num1<<endl;
+    cout<<"Number 2 = "<<num2<<endl;
+    cout<<"Number 3 = "<<num3<<endl;
+    cout<<"Number 4 = "<<num4<<endl;
+    cout<<"Number 5 = "<<num5<<endl;
+    cout<<"Number 6 = "<<num6<<endl;
+    cout<<"Character 1 = "<<ch1<<endl;
+    cout<<"Character 2 = "<<ch2<<endl;
+
+    getch();
+}
