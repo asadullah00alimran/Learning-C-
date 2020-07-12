b2958221a52bd80ec12c19f31a7d1c84f610d039
Asadullah Al Imran <66588286+asadullah00alimran@users.noreply.github.com>
2020-07-12 22:46:43 +0600

    Add files via upload

---
+++ 1 to N numbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,n;
+
+    cout<<"Enter the value of N (n= end line) : ";
+    cin>>n;
+
+    for(i=1;i<=n;i++)
+    {
+        cout<<i<<endl;
+    }
+
+    return 0;
+}
