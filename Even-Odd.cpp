73d91e3f4deb049e625f2c5acecbe0102e90b9c0
Asadullah Al Imran <66588286+asadullah00alimran@users.noreply.github.com>
2020-07-13 20:39:43 +0600

    Add files via upload

---
+++ Even-Odd.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout<<"Enter an integer number which you are want to check : ";
+    cin>>n;
+
+    if(n%2==0)
+    {
+        cout<<"\nThe number is Even\n";
+    }
+
+    else
+    {
+        cout<<"\nThe Number is Odd\n";
+    }
+
+
+    return 0;
+}
+
