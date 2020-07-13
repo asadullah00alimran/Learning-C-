2b3797f5e5551c2fb589c707a4660301767adf9a
Asadullah Al Imran <66588286+asadullah00alimran@users.noreply.github.com>
2020-07-13 20:40:11 +0600

    Add files via upload

---
+++ Increment-decrement operator.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x,y;
+
+    x = 10;
+
+    x++;
+    cout<<x<<endl;
+
+    ++x;
+    cout<<x<<endl;
+
+    y = --x;
+    cout<<y<<endl;
+
+
+    return 0 ;
+
+}
