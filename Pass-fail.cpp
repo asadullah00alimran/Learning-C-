92fafe08817c83b7aa56c9ad65759c9379c4b3e3
Asadullah Al Imran <66588286+asadullah00alimran@users.noreply.github.com>
2020-07-14 21:27:10 +0600

    Add files via upload

---
+++ Pass-fail.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout<<"Enter obtain number : ";
+    cin>>n;
+
+    if(n>=40)
+    {
+        cout<<"\nPASS\n";
+    }
+
+    else
+    {
+        cout<<"\nFAIL\n";
+    }
+
+
+    return 0;
+}
+
+
